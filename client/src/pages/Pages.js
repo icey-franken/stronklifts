0275dacfd89d7de7aacb6e7b176699f450724bd0
Isaac <finken.isaac@gmail.com>
2020-08-31 16:32:46 -0500

    Minor changes to Pages.js.

--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route } from "react-router-dom";
 import LoginPage from './LoginPage';
 
+//create protected routes!!
 export default function Pages() {
   return (
     <>
+			<Route exact path='/' component={HomePage} />
       <Route path='/login' component={LoginPage} />
     </>
   );
