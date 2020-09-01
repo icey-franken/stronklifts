c313a9af6bb5194147340d7c6f57a21daa44f1f8
Isaac <finken.isaac@gmail.com>
2020-09-01 18:39:58 -0500

    Working on rendering of workout/exercise components.

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
-
-
-
-
-export default function Workout () {
-	return (
-		<h1>I am a single workout component</h1>
-	)
+export default function Workout({ workout }) {
+  console.log(workout);
+  const { workoutDate: date, Exercises } = workout;
+	let note;
+  if (workout.WorkoutNote) {
+    const {
+      WorkoutNote: {description} ,
+		} = workout;
+		note = description;
+	}
+	let name;
+	if(Exercises.length > 0) {
+		//create an exercise component for each exercise in a workout
+		for(let i = 0; i < Exercises.length; i ++) {
+			const {
+				ExerciseName: { exerciseName },
+				WorkingWeight: {weight},
+				numRepsGoal,
+				numSets
+			} = Exercises[i];
+		}
+	}
+	console.log(date, note, name, Exercises);
+  debugger;
+  return <h1>I am a single workout component</h1>;
 }
