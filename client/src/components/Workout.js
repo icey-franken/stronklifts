782fdcf01dd75853b9d857ff9bb1bf0d17f074db
Isaac <finken.isaac@gmail.com>
2020-09-04 18:22:44 -0500

    Cleaned up workout component.

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -6,47 +6,7 @@ import "./Workout.css";
 export default function Workout({ workoutId }) {
 	// console.log(workout);
 	const workout = useSelector(state=> state.workouts[workoutId])
-	const exercises = useSelector((state)=>state.exercises);
-	const exercisesArr = Object.values(exercises);
 	if (!workout) return null;
-
-	const workoutExercisesArr = exercisesArr.filter(exercise=>{
-		return (exercise.workoutId === workout.id);
-	})
-
-  // let date;
-	// const { Exercises, workoutSplit: split } = workout;
-  // workout.workoutDate ? (date = workout.workoutDate) : (date = null);
-  // let note;
-  // if (workout.WorkoutNote) {
-  //   const {
-  //     WorkoutNote: { description },
-  //   } = workout;
-  //   note = description;
-  // }
-	// //do something with workout date and note
-
-	// //making big changes to store - in a bit we'll be able to pull exercises from store based on workoutId - all we have to pass to exercise component will be the workout id
-  // let exerciseArr = [];
-  // if (Exercises.length > 0) {
-  //   //create an exercise component for each exercise in a workout
-  //   for (let i = 0; i < Exercises.length; i++) {
-  //     const {
-  //       ExerciseName: { exerciseName },
-  //       WorkingWeight: { weight },
-  //       // numRepsGoal,
-  //       numSets,
-  //       Sets,
-  //     } = Exercises[i];
-  //     exerciseArr.push([
-  //       exerciseName.toUpperCase(),
-  //       weight,
-  //       Sets,
-  //       numSets,
-  //       split,
-  //     ]); //can also push numRepsGoal, BUT THIS IS A 5X5 APP. We only push numSets for the case of deadlift, where there is only 1 set
-  //   }
-  // }
 
   //date formatting
   const dateFormat = new Intl.DateTimeFormat("en", {
@@ -63,7 +23,6 @@ export default function Workout({ workoutId }) {
   let dateStr = "";
   dateArr.forEach((el) => (dateStr += el.value));
 	//date string day number is behind by 1 - what the fuck?
-
 
 	let note = 'need to make note slice of state'
   return (
