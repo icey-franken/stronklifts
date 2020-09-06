cb2bd45c0be823f1e0ead10eae2abb3105d4a35e
Isaac <finken.isaac@gmail.com>
2020-09-06 13:10:49 -0500

    Minor fixes to new lifter form. Will now add backend route for delete functionality on active workout page.

--- client/src/components/NewLifterForm.js
+++ client/src/components/NewLifterForm.js
@@ -8,7 +8,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
 import { createWorkoutThunk } from "../store/workouts";
 import { Redirect } from "react-router-dom";
-import ActiveWorkout from '../pages/ActiveWorkout';
+import ActiveWorkout from "../pages/ActiveWorkout";
 
 //lifted from auth page - in the future you should integrate this as an option
 const useStyles = makeStyles({
@@ -70,7 +70,9 @@ export default function NewLifterForm() {
   return (
     <>
       {WW ? (
-        <ActiveWorkout />
+        <Redirect to="/workout">
+          <ActiveWorkout />
+        </Redirect>
       ) : (
         <Container fixed maxWidth="sm" classes={{ root: classes.container }}>
           <SLLogo />
