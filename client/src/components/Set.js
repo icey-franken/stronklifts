0ee0638e138f22ea5d43dbea0889460b46ceb6ab
Isaac <finken.isaac@gmail.com>
2020-09-06 21:36:26 -0500

    Cleaned up wasSuccessful flag in set component.

--- client/src/components/Set.js
+++ client/src/components/Set.js
@@ -9,37 +9,43 @@ export default function Set({ setId }) {
   const sets = useSelector((state) => state.sets);
   const exerciseId = set.exerciseId;
   const setIds = useSelector((state) => state.exercises[exerciseId].setIds);
-  const numRepsGoal = useSelector(
-    (state) => state.exercises[exerciseId].numRepsGoal
-  );
-  const numSets = useSelector((state) => state.exercises[exerciseId].numSets);
   const dispatch = useDispatch();
 
+
   const handleClick = (e) => {
-    let num = e.target.innerHTML;
+		let num;
+		let success = null;
+		(e.target.innerHTML === '') ? (num = '') : (num = parseInt(e.target.innerHTML,10));
+
     if (num === "") {
-      num = "5";
-      e.target.classList.remove("exercise__set--emptySet");
 			set.numRepsActual = 5;
+      num = 5;
+			e.target.classList.remove("exercise__set--emptySet");
+			//we need this because use selector lags behind
       const successfulSets = setIds.filter((setId) => {
-        if (setId === "emptySet") return false;
+        if (setId === "emptySet") return true;
         return 5 === sets[setId].numRepsActual;
-        // return numRepsActual === numRepsGoal;
-      });
-      if (successfulSets.length === numSets) {
-        dispatch(updateExerciseSuccessThunk(exerciseId, true));
+			});
+      if (successfulSets.length === 5) {
+				success = true;
+        // dispatch(updateExerciseSuccessThunk(exerciseId, true));
       }
     } else if (num === 5) {
-      num--;
-      dispatch(updateExerciseSuccessThunk(exerciseId, false));
+			num--;
+			success = false;
+      // dispatch(updateExerciseSuccessThunk(exerciseId, false));
     } else if (num > 0) {
       num--;
-    } else if (num === "0") {
-      e.target.classList.add("exercise__set--emptySet");
+    } else if (num === 0) {
       num = null;
+      e.target.classList.add("exercise__set--emptySet");
     }
-    dispatch(updateRepsThunk(setId, num));
-    //this updates store - we should set something up that updates the value in the database after a certain amount of dead time. (debouncing?)
+		//this updates store - we should set something up that updates the value in the database after a certain amount of dead time. (debouncing?)
+		dispatch(updateRepsThunk(setId, num));
+
+		if(success!==null) {
+			dispatch(updateExerciseSuccessThunk(exerciseId, success));
+		}
   };
 
   return (
