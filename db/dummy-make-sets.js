6f009d8b2ab26870248d98aec91b082246a9b753
Isaac <finken.isaac@gmail.com>
2020-10-02 20:29:27 -0500

    Added seed data for june.

---
+++ db/dummy-make-sets.js
@@ -0,0 +1,143 @@
+function makeSets(startExId, numWorkouts) {
+  // end = startExId + numWorkouts*3
+  const end = startExId + (6 / 2) * numWorkouts;
+  let setsArr = [];
+  for (let i = startExId; i < end; i += 6) {
+    setsArr.push(
+      ...[
+        {
+          exerciseId: i,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 1,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 1,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 1,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 1,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 1,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 2,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 3,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 3,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 3,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 3,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 3,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 4,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 4,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 4,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 4,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 4,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 5,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 5,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 5,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 5,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: i + 5,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+      ]
+    );
+  }
+  return setsArr;
+}
+console.log(makeSets(25,4))
--- db/seeders/20200831011919-add-sets.js
+++ db/seeders/20200831011919-add-sets.js
@@ -1,148 +1,591 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Sets', [{
-			exerciseId: 1,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 1,
-			setOrder: 2,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 1,
-			setOrder: 3,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 1,
-			setOrder: 4,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 1,
-			setOrder: 5,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 2,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 2,
-			setOrder: 2,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 2,
-			setOrder: 3,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 2,
-			setOrder: 4,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 2,
-			setOrder: 5,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 3,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 4,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 4,
-			setOrder: 2,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 4,
-			setOrder: 3,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 4,
-			setOrder: 4,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 4,
-			setOrder: 5,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 5,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 5,
-			setOrder: 2,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 5,
-			setOrder: 3,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 5,
-			setOrder: 4,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 5,
-			setOrder: 5,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 6,
-			setOrder: 1,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 6,
-			setOrder: 2,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 6,
-			setOrder: 3,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 6,
-			setOrder: 4,
-			numRepsActual: 5,
-		},
-		{
-			exerciseId: 6,
-			setOrder: 5,
-			numRepsActual: 5,
-		},
-	], {});
+    return queryInterface.bulkInsert(
+      "Sets",
+      [
+        {
+          exerciseId: 1,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 1,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 1,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 1,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 1,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 2,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 2,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 2,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 2,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 2,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 3,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 4,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 4,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 4,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 4,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 4,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 5,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 5,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 5,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 5,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 5,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 6,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 6,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 6,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 6,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 6,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 7,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 7,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 7,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 7,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 7,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 8,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 8,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 8,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 8,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 8,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 9,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 10,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 10,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 10,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 10,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 10,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 11,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 11,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 11,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 11,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 11,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 12,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 12,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 12,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 12,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 12,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 13,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 13,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 13,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 13,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 13,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 14,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 14,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 14,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 14,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 14,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 15,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 16,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 16,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 16,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 16,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 16,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 17,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 17,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 17,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 17,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 17,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 18,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 18,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 18,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 18,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 18,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 19,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 19,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 19,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 19,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 19,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 20,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 20,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 20,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 20,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 20,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 21,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 22,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 22,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 22,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 22,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 22,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 23,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 23,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 23,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 23,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 23,
+          setOrder: 5,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 24,
+          setOrder: 1,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 24,
+          setOrder: 2,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 24,
+          setOrder: 3,
+          numRepsActual: 5,
+        },
+        {
+          exerciseId: 24,
+          setOrder: 4,
+          numRepsActual: 5,
+        },
+        { exerciseId: 24, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 25, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 25, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 25, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 25, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 25, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 26, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 26, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 26, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 26, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 26, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 27, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 28, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 28, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 28, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 28, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 28, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 29, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 29, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 29, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 29, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 29, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 30, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 30, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 30, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 30, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 30, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 31, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 31, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 31, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 31, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 31, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 32, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 32, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 32, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 32, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 32, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 33, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 34, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 34, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 34, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 34, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 34, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 35, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 35, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 35, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 35, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 35, setOrder: 5, numRepsActual: 5 },
+        { exerciseId: 36, setOrder: 1, numRepsActual: 5 },
+        { exerciseId: 36, setOrder: 2, numRepsActual: 5 },
+        { exerciseId: 36, setOrder: 3, numRepsActual: 5 },
+        { exerciseId: 36, setOrder: 4, numRepsActual: 5 },
+        { exerciseId: 36, setOrder: 5, numRepsActual: 5 },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-		return queryInterface.bulkDelete('Sets', null, {});
-		/*
+    return queryInterface.bulkDelete("Sets", null, {});
+    /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
 
     */
-  }
+  },
 };
