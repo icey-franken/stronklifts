97dbaf299be62ae434e0fd54ca03ed924d4391d5
Isaac <finken.isaac@gmail.com>
2020-10-05 10:22:28 -0500

    Fixed logout button.

--- client/src/components/utils/LogoutButton.js
+++ client/src/components/utils/LogoutButton.js
@@ -2,13 +2,13 @@ import React from "react";
 // import {Button} from '@material-ui/core';
 import { useDispatch } from "react-redux";
 import { NavLink, Redirect } from "react-router-dom";
-import { AuthActions } from "../../store/auth";
+import { AuthThunks } from "../../store/auth";
 
 export default function LogoutButton() {
   const dispatch = useDispatch();
 
   const handleClick = () => {
-    dispatch(AuthActions.logout());
+    dispatch(AuthThunks.logout());
     return <Redirect to="/login" />;
   };
 
